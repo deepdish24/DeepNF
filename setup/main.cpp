89b71a2d9b087df0c4fe93c2f39d7fbf20cfe781
Deepan Saravanan <deepan.saravanan83@gmail.com>
2017-12-02 21:20:59 -0500

    testing update

--- setup/main.cpp
+++ setup/main.cpp
@@ -4,13 +4,21 @@
 */
 
 #include <iostream>
+#include <fstream>
 #include "json.hpp"
 
 using json = nlohmann::json;
 
 int main(int argc, char *argv[]) {
+    if (argc == 0) {
+        std::cerr << "No input json file given\n";
+        exit(1);
+    }
     json j;
-    j["pi"] = 3.14;
+    std::string filepath(argv[1]);
+    std::ifstream fileInput(filepath);
+    j << fileInput;
+    
     std::cout << j["pi"] << "\n";
     std::cout << "Deepan Saravanan (deepans)";
     return 0;
