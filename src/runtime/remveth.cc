d675f2d5ff9c76665b41d18ee7adeb8c8d918d39
Akash Subramanian <akashsub@seas.upenn.edu>
2018-02-23 14:08:49 -0500

    updated remveth.cc to delete veth* interfaces

--- src/runtime/remveth.cc
+++ src/runtime/remveth.cc
@@ -5,12 +5,25 @@
 #include <stdio.h>
 #include <string.h>
 
-int main() { system(("ifconfig > a.txt"));
-    std::ifstream mbox_file("a.txt");
-	        	std::string line;
-	        	if (mbox_file.is_open()) {int i;
-                 while(getline(mbox_file, line)) { if ((i = line.find("_")) != std::string::npos) { std::string v = line.substr(0, i+2); if (v != "gre_s") {system(("sudo ip link delete " + v + " type veth").c_str()); } } } }        
+int main() { 
+	system(("ifconfig > a.txt"));
+	std::ifstream mbox_file("a.txt");
+	std::string line;
+	if (mbox_file.is_open()) {
+		int i;
+		while(getline(mbox_file, line)) {
+			if ((i = line.find("_")) != std::string::npos) {
+				std::string v = line.substr(0, i+2); 
+				if (v != "gre_s") {
+					system(("sudo ip link delete " + v).c_str());
+				} 
+			} else if ((i = line.find("veth")) == 0) {
+				int space_index = line.find(" ");
+				std::string v = line.substr(0, space_index);
+				system(("sudo ip link delete " + v).c_str());
+			} 
+		} 
+	}        
 
-
-    return 0;
+	return 0;
 }
