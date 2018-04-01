32b4f47a74f1dd75d582a5574c1e699e510c3869
Victoria <xiaov@levy-ve506-0520.apn.wlan.upenn.edu>
2018-04-01 01:43:42 -0400

    adding json to build.sh

--- src/runtime/merger/build.sh
+++ src/runtime/merger/build.sh
@@ -2,9 +2,9 @@ g++ -std=c++11 -c NF.h NF.cpp
 g++ -std=c++11 -c Field.h Field.cpp
 g++ -std=c++11 -c RuntimeNode.h RuntimeNode.cpp
 g++ -std=c++11 -c packet.h packet.cpp
-g++ -std=c++11 -c ActionTableHelper.h ActionTableHelper.cpp
+g++ -std=c++11 -c -I//.linuxbrew/Cellar/nlohmann_json/3.1.2/include ActionTableHelper.h ActionTableHelper.cpp
 g++ -std=c++11 -c ConflictItem.h ConflictItem.cpp
 g++ -std=c++11 -c MergerInfo.h MergerInfo.cpp
-g++ -std=c++11 MergerOperator.h MergerOperator.h
+g++ -std=c++11 -c -I//.linuxbrew/Cellar/nlohmann_json/3.1.2/include MergerOperator.h MergerOperator.h
 
-g++ -std=c++11 NF.cpp packet.cpp RuntimeNode.cpp ActionTableHelper.cpp ConflictItem.cpp MergerInfo.cpp MergerOperator.cpp merge.cpp -o merge -I//.linuxbrew/Cellar/nlohmann_json/3.1.2/include -I /usr/include/pcap -lpcap
+g++ -std=c++11 -I//.linuxbrew/Cellar/nlohmann_json/3.1.2/include NF.cpp packet.cpp RuntimeNode.cpp ActionTableHelper.cpp ConflictItem.cpp MergerInfo.cpp MergerOperator.cpp merge.cpp -o merge -I /usr/include/pcap -lpcap
