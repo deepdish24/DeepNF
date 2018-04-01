edb10e189da35656634011b61173c6e32f1af1fb
Victoria <xiaov@levy-ve506-0520.apn.wlan.upenn.edu>
2018-04-01 19:33:48 -0400

    fixed compile errors

--- src/runtime/merger/build.sh
+++ src/runtime/merger/build.sh
@@ -7,4 +7,4 @@ g++ -std=c++11 -c ConflictItem.h ConflictItem.cpp
 g++ -std=c++11 -c MergerInfo.h MergerInfo.cpp
 g++ -std=c++11 -c -I//.linuxbrew/Cellar/nlohmann_json/3.1.2/include MergerOperator.h MergerOperator.h
 
-g++ -std=c++11 -I//.linuxbrew/Cellar/nlohmann_json/3.1.2/include NF.cpp packet.cpp RuntimeNode.cpp ActionTableHelper.cpp ConflictItem.cpp MergerInfo.cpp MergerOperator.cpp merge.cpp -o merge -I /usr/include/pcap -lpcap
+g++ -std=c++11 -I//.linuxbrew/Cellar/nlohmann_json/3.1.2/include NF.cpp Field.cpp packet.cpp RuntimeNode.cpp ActionTableHelper.cpp ConflictItem.cpp MergerInfo.cpp MergerOperator.cpp merge.cpp -o merge -I /usr/include/pcap -lpcap
