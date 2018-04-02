012f4d6dadaee22f53d0da0e8ad216f08f031069
Victoria <xiaov@levy-ve506-0520.apn.wlan.upenn.edu>
2018-04-02 03:47:25 -0400

    test dropping null packets

--- src/runtime/merger/MergerOperator.cpp
+++ src/runtime/merger/MergerOperator.cpp
@@ -91,7 +91,7 @@ void MergerOperator::process_packet(u_char *arg,
     NFPacket* p = new NFPacket();
     p->pkt = pkt_info;
 
-    printf("cur_dev: %s", this->cur_dev);
+    printf("cur_dev: %s", this->cur_dev.c_str());
     RuntimeNode* n = this->merger_info->get_interface_leaf_map().at(cur_dev);
     printf("this runtimeNode: %d\n", n->get_id());
 
