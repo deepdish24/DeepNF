a3e26eedb1f910dfee40504cd48d8338cbbcbf65
Jiahui Jiao <jiaoj@seas.upenn.edu>
2018-04-07 05:29:36 -0400

    read from multiple files

--- src/webserver/routes/routes.js
+++ src/webserver/routes/routes.js
@@ -21,17 +21,131 @@ var printData = function(req, res){
         //console.log(contents);
 
         
-        res.render("bs.ejs", {content_data: contents});
+        //res.render("bs.ejs", {content_data: contents});
       });
  
     console.log('after calling readFile');
 
 }
 
+var fs = require('fs');
+
+
+
+function readFiles(dirname, onFileContent, onError) {
+
+  fs.readdir(dirname, function(err, filenames) {
+  	//return filenames.length;
+    if (err) {
+      onError(err);
+      return;
+    }
+    var data = {};
+    filenames.forEach(function(filename) {
+    	
+      fs.readFile(dirname + filename, 'utf-8', function(err, content) {
+        if (err) {
+          onError(err);
+          return;
+        }
+            var contents_array = content.split('\n');
+        	var i = 0
+        	var content_object = {};
+        	for (var key in contents_array){
+        	
+        		var data_array = contents_array[key].split(',');
+        		content_object[i] = {time:data_array[0], packetID:data_array[1], function:data_array[2], action:data_array[3]};
+        		i = i+1;
+        		//console.log(content_object);
+        	}
+        	data[filename] = content_object;
+        	//console.log(Object.keys(data).length);
+        if (Object.keys(data).length == filenames.length) {
+        	//console.log(data);
+        	onFileContent(data);
+        }
+        
+        //onFileContent(filename, content);
+      });
+    });
+  });
+}
+
+function getNumFiles(dirname){
+	fs.readdir(dirname, function(err, filenames) {
+		console.log(filenames.length);
+  	return filenames.length;
+  });
+}
+
+function getFiles() {
+
+}
+
 var showMachine1 = function(req, res) {
-		var fs = require('fs');
-	var content_object = {};
-	 fs.readFile('test.txt', 'utf8', function(err, contents) {
+	var dirname = './outputs/machine1/';
+	var data = {};
+	readFiles(dirname, function(callback) {
+
+  		data = callback;
+  		console.log(data);
+  		//console.log(numFiles);
+
+  		res.render("machine1.ejs", {content_data: data});
+  		//return data;
+	}, function(err) {
+  		throw err;
+	});
+	//var request2 = fetch('/articles.json');
+
+	//console.log(request1);
+	var data = {};
+	
+	//console.log(data);
+
+	
+	
+
+
+//console.log(data); 
+
+  	/*fs.readdir(dirname, function(err, filenames) {
+    	if (err) {
+      		return;
+    	}
+
+
+    	for (var a = 0; a < filenames.length; a++) {
+      		fs.readFile(dirname + filenames[a], 'utf-8', function(err, contents) {
+        		if (err) {
+        			console.log(err);
+          			return;
+        		}
+        		
+        		//console.log(contents);
+        	var contents_array = contents.split('\n');
+        	var i = 0
+        	var content_object = {};
+        	for (var key in contents_array){
+        	
+        		var data_array = contents_array[key].split(',');
+        		content_object[i] = {time:data_array[0], packetID:data_array[1], function:data_array[2], action:data_array[3]};
+        		i = i+1;
+        		//console.log(content_object);
+        	}
+        	data[filenames[a]] = content_object;
+        	console.log(data);
+        	
+        	
+      });
+      		console.log(data);
+    };
+console.log(data);
+  	res.render("machine1.ejs", {content_data: data});
+  });*/
+  	
+
+/*	 fs.readFile('test.txt', 'utf8', function(err, contents) {
       	console.log("currently reading file");
         
 
@@ -46,8 +160,9 @@ var showMachine1 = function(req, res) {
         }
         
         res.render("machine1.ejs", {content_data: content_object});
-      });
+      });*/
 }
+
 
 /*var getDataMachine1 = function(req, res) {
 	var fs = require('fs');
