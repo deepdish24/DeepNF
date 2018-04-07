a788ac5b7da40fa83f626417c84b0167d37fdfbc
Victoria <xiaov@levy-ve503-0572.apn.wlan.upenn.edu>
2018-04-07 14:07:33 -0400

    ripp

--- src/runtime/forwarder/null_tester.cpp
+++ src/runtime/forwarder/null_tester.cpp
@@ -29,37 +29,33 @@ int main(int argc, char *argv[]) {
         // prepare destination address
 
         std::string addr_str = stringify(std::string(ip), portno);
-//        char *addr_str = (char *) malloc(strlen(ip) + strlen(argv[2]) + 2);
-//        strncpy(addr_str, ip, strlen(ip));
-//        strcat(addr_str, ":");
-//        strcat(addr_str, argv[3]);
         std::cout << "address to send to " << addr_str << "\n";
         address *addr = address_from_string(addr_str);
-//
-//        // prepare packet
-//        std::string data("null packetto");
-//        std::string sip = "127.0.0.1";
-//        int sp = 7777;
-//        std::string dip(argv[3]);
-//        int dp = portno;
-//        srand(time(NULL));
-//        struct packet p(sip, sp, dip, dp, (unsigned short) 17, data);
-//        printf("before nullifying buf: %02X\n", p.pkt);
-//
-//        printf("Going to nullify packet now\n");
-//        p.nullify();
-//        bool is_null = p.is_null();
-//        if (is_null) {
-//            printf("null\n");
-//        } else {
-//            printf("not null\n");
-//        }
-//
-//        if (send_packet(&p, sockfd, addr) < 0) {
-//            fprintf(stderr, "Send packet error: %s", strerror(errno));
-//            exit(-1);
-//        }
-//        printf("Sent null packet\n");
+
+        // prepare packet
+        std::string data("null packetto");
+        std::string sip = "127.0.0.1";
+        int sp = 7777;
+        std::string dip(argv[3]);
+        int dp = portno;
+        srand(time(NULL));
+        struct packet p(sip, sp, dip, dp, (unsigned short) 17, data);
+        printf("before nullifying buf: %02X\n", p.pkt);
+
+        printf("Going to nullify packet now\n");
+        p.nullify();
+        bool is_null = p.is_null();
+        if (is_null) {
+            printf("null\n");
+        } else {
+            printf("not null\n");
+        }
+
+        if (send_packet(&p, sockfd, addr) < 0) {
+            fprintf(stderr, "Send packet error: %s", strerror(errno));
+            exit(-1);
+        }
+        printf("Sent null packet\n");
     }
 
     else {
