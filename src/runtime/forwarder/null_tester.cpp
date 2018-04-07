a9cdd6ab5d26792cca0863f72b3a67eb17e87749
Victoria <xiaov@levy-ve504-0317.apn.wlan.upenn.edu>
2018-04-06 23:00:33 -0400

    resolve_packet_conflict with null packets

--- src/runtime/forwarder/null_tester.cpp
+++ src/runtime/forwarder/null_tester.cpp
@@ -46,7 +46,9 @@ int main(int argc, char *argv[]) {
         p.is_null();
 
         struct packet new_p(p.pkt, p.size);
+        printf("struct packet new_p(p.pkt, p.size);\n");
         p.is_null();
+        printf("p.is_null();\n");
 
         if (send_packet(&p, sockfd, addr) < 0) {
             fprintf(stderr, "Send packet error: %s", strerror(errno));
