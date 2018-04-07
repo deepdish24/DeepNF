957732479a5a2cb918ba77e7ab23bacc0f015c74
Jiahui Jiao <jiaoj@seas.upenn.edu>
2018-04-07 06:29:11 -0400

    website setup with read files

--- src/webserver/routes/routes.js
+++ src/webserver/routes/routes.js
@@ -1,193 +1,98 @@
-/** TODO: CHANGE DUMMY USER ID TO CURRENT USER ID
- * Get the current user id and render the home feed
- */
+var fs = require('fs');
+// Get the home page ('/')
 var getHome = function(req, res){
-
-	//TODO: Change to current user
-	//var current_user_id = "1";
-
-	res.render("index.ejs");
+	res.render("home.ejs");
 }; 
 
-var showBS = function(req, res){
-	res.render("bs.ejs");
+// Get about page
+var getAbout = function(req, res) {
+	res.render("about.ejs");
 }
 
-var printData = function(req, res){
-	var fs = require('fs');
- 
-      fs.readFile('test.txt', 'utf8', function(err, contents) {
-      	console.log("currently reading file");
-        //console.log(contents);
-
-        
-        //res.render("bs.ejs", {content_data: contents});
-      });
- 
-    console.log('after calling readFile');
-
-}
-
-var fs = require('fs');
-
-
-
+// Function to read all text files inside a given directory. Assuming the text file has
+// (time,packetID,function,action) format, this function construct a javascript object
 function readFiles(dirname, onFileContent, onError) {
-
   fs.readdir(dirname, function(err, filenames) {
-  	//return filenames.length;
     if (err) {
       onError(err);
       return;
     }
-    var data = {};
+    var data = {}; // object containing all text files results
+    if (filenames.length == 0) {
+    	onFileContent(undefined);
+    }
+    // Read all files in the directory
     filenames.forEach(function(filename) {
-    	
       fs.readFile(dirname + filename, 'utf-8', function(err, content) {
         if (err) {
           onError(err);
           return;
         }
-            var contents_array = content.split('\n');
-        	var i = 0
-        	var content_object = {};
-        	for (var key in contents_array){
-        	
-        		var data_array = contents_array[key].split(',');
-        		content_object[i] = {time:data_array[0], packetID:data_array[1], function:data_array[2], action:data_array[3]};
-        		i = i+1;
-        		//console.log(content_object);
-        	}
-        	data[filename] = content_object;
-        	//console.log(Object.keys(data).length);
-        if (Object.keys(data).length == filenames.length) {
-        	//console.log(data);
-        	onFileContent(data);
-        }
+       	  var contents_array = content.split('\n'); // Split text files by new line
+          var i = 0; // Check if all files have been processed
+          var content_object = {}; // object containing results for 1 text file
+          for (var key in contents_array){
+        	var data_array = contents_array[key].split(','); // Get each field
+        	content_object[i] = {time:data_array[0], packetID:data_array[1], function:data_array[2], action:data_array[3]};
+        	i = i+1;
+          }
+          data[filename] = content_object; // Insert the textfile into the array we are passing on
         
-        //onFileContent(filename, content);
+          // If every text file has been processed, send the object to call back.
+          if (Object.keys(data).length == filenames.length) {
+        	onFileContent(data);
+          }
       });
     });
   });
 }
 
-function getNumFiles(dirname){
-	fs.readdir(dirname, function(err, filenames) {
-		console.log(filenames.length);
-  	return filenames.length;
-  });
-}
-
-function getFiles() {
-
-}
-
+// Send machine 1's log data to the webpage
 var showMachine1 = function(req, res) {
-	var dirname = './outputs/machine1/';
+	var dirname = './outputs/machine1/'; // directory path 
 	var data = {};
 	readFiles(dirname, function(callback) {
-
-  		data = callback;
-  		console.log(data);
-  		//console.log(numFiles);
-
-  		res.render("machine1.ejs", {content_data: data});
-  		//return data;
+  	  data = callback;
+      res.render("machine1.ejs", {content_data: data});
 	}, function(err) {
   		throw err;
 	});
-	//var request2 = fetch('/articles.json');
+}
 
-	//console.log(request1);
+// Send machine 2's log data to the webpage
+var showMachine2 = function(req, res) {
+	var dirname = './outputs/machine2/'; // directory path 
 	var data = {};
-	
-	//console.log(data);
+	readFiles(dirname, function(callback) {
+  	  data = callback;
+      res.render("machine2.ejs", {content_data: data});
+	}, function(err) {
+  		throw err;
+	});
+}
 
-	
-	
+// Send machine 2's log data to the webpage
+var showMachine3 = function(req, res) {
+	var dirname = './outputs/machine3/'; // directory path 
+	var data = {};
+	readFiles(dirname, function(callback) {
+  	  data = callback;
 
-
-//console.log(data); 
-
-  	/*fs.readdir(dirname, function(err, filenames) {
-    	if (err) {
-      		return;
-    	}
-
-
-    	for (var a = 0; a < filenames.length; a++) {
-      		fs.readFile(dirname + filenames[a], 'utf-8', function(err, contents) {
-        		if (err) {
-        			console.log(err);
-          			return;
-        		}
-        		
-        		//console.log(contents);
-        	var contents_array = contents.split('\n');
-        	var i = 0
-        	var content_object = {};
-        	for (var key in contents_array){
-        	
-        		var data_array = contents_array[key].split(',');
-        		content_object[i] = {time:data_array[0], packetID:data_array[1], function:data_array[2], action:data_array[3]};
-        		i = i+1;
-        		//console.log(content_object);
-        	}
-        	data[filenames[a]] = content_object;
-        	console.log(data);
-        	
-        	
-      });
-      		console.log(data);
-    };
-console.log(data);
-  	res.render("machine1.ejs", {content_data: data});
-  });*/
-  	
-
-/*	 fs.readFile('test.txt', 'utf8', function(err, contents) {
-      	console.log("currently reading file");
-        
-
-        var contents_array = contents.split('\n');
-        var i = 0
-        for (var key in contents_array){
-        	
-        	var data_array = contents_array[key].split(',');
-        	content_object[i] = {time:data_array[0], packetID:data_array[1], function:data_array[2], action:data_array[3]};
-        	i = i+1;
-        	//console.log(content_object);
-        }
-        
-        res.render("machine1.ejs", {content_data: content_object});
-      });*/
+      res.render("machine3.ejs", {content_data: data});
+	}, function(err) {
+  		throw err;
+	});
 }
 
 
-/*var getDataMachine1 = function(req, res) {
-	var fs = require('fs');
-	var content_object = {};
-	 fs.readFile('test.txt', 'utf8', function(err, contents) {
-      	console.log("currently reading file");
-        console.log(contents);
-
-        var contents_array = contents.split('\n');
-        for (var i = 0; i < contents_array.length(); i++) {
-        	var data_array = contents_array[i].split(',');
-        	content_object[i] = {time:data_array[0], function_id:data_array[1], function:data_array[2], action:data_array[3]};
-        }
-        
-        res.({content_data: content_object});
-      });
-}
-*/
 //Set routes
 var routes = { 
 
 		get_home: getHome,
-		print_Data: printData,
-		//get_Data: getData,
+		get_about: getAbout,
 		show_machine1: showMachine1,
+		show_machine2: showMachine2,
+		show_machine3: showMachine3,
 		//get_data_machine1: getDataMachine1
 };
 
