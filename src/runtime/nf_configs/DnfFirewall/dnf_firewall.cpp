e5c07a08f057c50a90c9f5f0f91cb0d9637d1c96
Victoria <xiaov@levy-ve502-0663.apn.wlan.upenn.edu>
2018-04-09 01:13:31 -0400

    firewall writes to log

--- src/runtime/nf_configs/DnfFirewall/dnf_firewall.cpp
+++ src/runtime/nf_configs/DnfFirewall/dnf_firewall.cpp
@@ -53,6 +53,7 @@ int main(int argc,char **argv)
     // setup log for this NF
     std::ofstream log;
     log.open("/log/log.txt", std::ios::out);
+    if (!log) std::cerr << "Could not open the file!" << std::endl;
 
     // create socket
     int sockfd = open_socket();
